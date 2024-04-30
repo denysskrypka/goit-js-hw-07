4c4c62f57d46e8156fcc1077e11252125265c3ed
denysskrypka <118266939+denysskrypka@users.noreply.github.com>
2024-04-30 15:07:16 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,7 +1,7 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-        .toString(16)
-        .padStart(6, '0')}`;
+  return `#${Math.floor(Math.random() * 16777215)
+   .toString(16)
+   .padStart(6, '0')}`;
 }
 
 const controls = document.querySelector("#controls");
@@ -30,7 +30,8 @@ function createBoxes() {
     fragment.appendChild(div);
     boxSize += 10;
   }
-  boxes.appendChild(fragment);
+  boxes.append(...fragment.children);
+  fragment.innerHTML = "";
   input.value = "";
 }
 
